c94316dd0b42cdc9ad55064bc50e4636d7b66b0f
CESAR RODRIGUES COUTO <cesar.rcouto@senacsp.edu.br>
2024-04-25 12:56:33 -0300

    Aula debs

--- agencia_de_viagens/src/assets/Componentes/Aruba.jsx
+++ agencia_de_viagens/src/assets/Componentes/Aruba.jsx
@@ -1,9 +1,17 @@
 import Styles from './Aruba.module.css'
+import ComponenteProps from './ComponenteProps'
+import Bandeira_Aruba from './imagens/bandeira_aruba.jfif'
 
 function Aruba(){
     return(
         <div className={Styles.fundo}>
-            <h1>Venha conhecer as maravilhas de aruba</h1>
+          <ComponenteProps 
+           lugar="Paises Baixos"
+           texto="Bem-vindo ao destino dos sonhos, onde praias de areias brancas se encontram com águas cristalinas em tons de azul-turquesa. Em Aruba, você será recebido pelo caloroso sol do Caribe e pela brisa suave que acaricia sua pele, convidando-o a explorar um paraíso tropical como nenhum outro."
+           bandeira={Bandeira_Aruba}
+          
+          
+          />
         </div>
     )
 }
--- agencia_de_viagens/src/assets/Componentes/Muralhas_da_China.jsx
+++ agencia_de_viagens/src/assets/Componentes/Muralhas_da_China.jsx
@@ -1,9 +1,16 @@
+import ComponenteProps from './ComponenteProps'
 import Styles from './Muralhas_da_China.module.css'
+import Bandeira_China from './imagens/bandeira_china.jfif'
 
 function Muralhas_da_China(){
     return(
         <div className={Styles.fundo}>
-            <h1>Venha conhecer uma das 7 maravihas do mundo</h1>
+           <ComponenteProps 
+           lugar="China"
+           texto="Bem-vindo a um dos tesouros mais impressionantes da humanidade: as majestosas Muralhas da China. Prepare-se para ser transportado através dos séculos enquanto explora esta maravilha arquitetônica que permanece como um testemunho vivo da história e da grandiosidade da civilização chinesa."
+           bandeira={Bandeira_China}
+           
+           />
         </div>
     )
 }
